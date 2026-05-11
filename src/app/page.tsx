926fc19051f1c1d25ea412577ebd9005c84a3b0e
Stanlej95 <goral.stanislaw95@gmail.com>
2026-05-11 11:03:26 +0200

    added mobile styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client" // Kluczowe w Next.js dla komponentów interaktywnych
+"use client"
 
 import Sidebar from "@/components/Sidebar"
 import Render from "@/components/Render"
@@ -7,18 +7,21 @@ import { useWardrobe } from '@/hooks/useWardrobe'
 export default function Home() {
     const { wardrobe, handleUpdate, addShelf, removeShelf } = useWardrobe()
 
-    
     return (
-        <main className="flex min-h-screen"> 
-            {/* Używamy Tailwind CSS zamiast app-container z CSS */}
-            <Sidebar
-                wardrobe = {wardrobe}
-                onUpdate={handleUpdate}
-                onAddShelf={addShelf}
-                onRemoveShelf={removeShelf}
-            />
+  
+        <main className="flex flex-col lg:flex-row min-h-screen bg-neutral-900 overflow-hidden"> 
+            
+        
+            <div className="z-10 w-full lg:w-96 lg:min-h-screen border-b lg:border-r border-white/10 bg-black/20 backdrop-blur-md">
+                <Sidebar
+                    wardrobe={wardrobe}
+                    onUpdate={handleUpdate}
+                    onAddShelf={addShelf}
+                    onRemoveShelf={removeShelf}
+                />
+            </div>
 
-            <div className="flex-1 relative">
+            <div className="flex-1 relative h-[60vh] lg:h-screen">
                 <Render wardrobe={wardrobe} />
             </div>
         </main>
