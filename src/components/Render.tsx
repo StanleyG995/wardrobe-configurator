817f3841ffeb4c11558bca6d1eedc9eac7af2918
Stanislaw <goral.stanislaw95@gmail.com>
2026-06-02 18:57:35 +0200

    fixed shadows

--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -63,15 +63,18 @@ const Render = ({
 				}}>
 				<ambientLight intensity={0.3} />
 				<directionalLight
-					position={[1, 3, 4]}
+					position={[2, 6, 6]}
 					shadow-mapSize={[2048, 2048]}
 					intensity={1}
 					castShadow
 					shadow-radius={5}
-					shadow-camera-left={-1.5}
-					shadow-camera-right={1.5}
-					shadow-camera-top={2.5}
-					shadow-camera-bottom={-0.5}
+					shadow-camera-left={-4}
+					shadow-camera-right={4}
+					shadow-camera-top={4}
+					shadow-camera-bottom={-1}
+					shadow-camera-near={0.1}
+					shadow-camera-far={20}
+					shadow-bias={-0.0005}
 				/>
 				<hemisphereLight
 					intensity={0.5}
