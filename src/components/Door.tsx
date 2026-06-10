f7cc5659947ca8e94ab82a1a6480d41398be97a5
Stanlej95 <goral.stanislaw95@gmail.com>
2026-06-10 12:49:31 +0200

    fixed doortypes

--- src/components/Door.tsx
+++ src/components/Door.tsx
@@ -5,7 +5,7 @@ import { toMeters } from '@/helpers/unitConverter'
 const Door = ({width, height, depth, boardThickness}:Omit<WardrobeDimensions,'backBoardThickness'>) => {
 
     return (
-        <group position={[toMeters(width/2), 0, toMeters(depth/2+boardThickness/2)]} rotation = {[0,-Math.PI/1.5,0]}>
+        <group position={[toMeters(-width/2), 0, toMeters(depth/2+boardThickness/2)]} rotation = {[0,-Math.PI/1.5,0]}>
             <Board name='door' w={width} h={height} d={boardThickness} x={width/2} y={height/2} z={0} rotation = {[0,0,0]}/>
         </group>
         
