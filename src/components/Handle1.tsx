20f9b4080c2c225c0428e4255e40dc4355af60a1
Stanlej95 <goral.stanislaw95@gmail.com>
2026-06-10 15:35:46 +0200

    added door handles

--- src/components/Handle1.tsx
+++ src/components/Handle1.tsx
@@ -1,21 +1,19 @@
 import * as THREE from 'three'
 import React from 'react'
 import { useGLTF } from '@react-three/drei'
-import { GLTF } from 'three-stdlib'
 import { ThreeElements } from '@react-three/fiber'
 
-type GLTFResult = GLTF & {
-  nodes: {
-    Cube: THREE.Mesh
-  }
-  materials: {}
-}
-
 function Handle1(props: ThreeElements['group']) {
-  const { nodes, materials } = useGLTF('/models/handle-1.glb') as GLTFResult
+
+  const { nodes } = useGLTF('/models/handle-1.glb')
+
+  const handleMesh = nodes.Cube as THREE.Mesh
+
+  if (!handleMesh) return null
+
   return (
     <group {...props} dispose={null}>
-      <mesh geometry={nodes.Cube.geometry} material={nodes.Cube.material} />
+      <mesh geometry={handleMesh.geometry} material={handleMesh.material} />
     </group>
   )
 }
