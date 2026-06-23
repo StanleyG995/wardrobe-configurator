2bd19e232f7ccf64f881bdf46416bdac53dc746e
Stanislaw <goral.stanislaw95@gmail.com>
2026-06-23 07:02:08 +0200

    refactor(human): optimize billboard rotation and document component constants

--- src/components/scene/environment/Floor.tsx
+++ src/components/scene/environment/Floor.tsx
@@ -9,26 +9,24 @@ const FLOOR_ROTATION: [number, number, number] = [-Math.PI / 2, 0, 0]
 const TEXTURE_REPEAT: [number, number] = [10, 10]
 
 const Floor = () => {
-   
-    const floorTexture = useTexture("/floor.png", (texture) => {
-        texture.wrapS = THREE.RepeatWrapping
-        texture.wrapT = THREE.RepeatWrapping
-        texture.repeat.set(...TEXTURE_REPEAT)
-        texture.colorSpace = THREE.SRGBColorSpace
-    })
+	const floorTexture = useTexture("/floor.png", texture => {
+		texture.wrapS = THREE.RepeatWrapping
+		texture.wrapT = THREE.RepeatWrapping
+		texture.repeat.set(...TEXTURE_REPEAT)
+		texture.colorSpace = THREE.SRGBColorSpace
+	})
 
-    return (
-        <mesh
-            name='floor'
-            scale={FLOOR_SCALE}
-            position={FLOOR_POSITION}
-            rotation={FLOOR_ROTATION}
-            receiveShadow
-        >
-            <planeGeometry />
-            <meshStandardMaterial map={floorTexture} side={THREE.DoubleSide} />
-        </mesh>
-    )
+	return (
+		<mesh
+			name='floor'
+			scale={FLOOR_SCALE}
+			position={FLOOR_POSITION}
+			rotation={FLOOR_ROTATION}
+			receiveShadow>
+			<planeGeometry />
+			<meshStandardMaterial map={floorTexture} side={THREE.DoubleSide} />
+		</mesh>
+	)
 }
 
 useTexture.preload("/floor.png")
--- src/components/scene/environment/HumanScale.tsx
+++ src/components/scene/environment/HumanScale.tsx
@@ -3,36 +3,41 @@
 import type { HumanScaleProps } from "@/types/RenderProps"
 import { useRef } from "react"
 import { useFrame } from "@react-three/fiber"
-import { Image as DreiImage, Line, Html } from "@react-three/drei"
-import { useTexture } from "@react-three/drei" // <-- Importujemy useTexture
+import { Image as DreiImage, Line, Html, useTexture } from "@react-three/drei"
 import { toMeters, toMilimeters } from "@/helpers/unitConverter"
 import * as THREE from "three"
 
-const ASPECT_RATIO = 0.268;
+const ASPECT_RATIO = 0.268
+const MALE_HEIGHT_MM = 1800
+const FEMALE_HEIGHT_MM = 1650
+const DIMENSION_OFFSET = 0.4
+
+const LINE_COLOR = "black"
+const LINE_WIDTH = 1
+const COMPONENT_POSITION: [number, number, number] = [0, 0, 1.4]
 
 const HumanScale = ({ gender, dimensions }: HumanScaleProps) => {
     const groupRef = useRef<THREE.Group>(null)
 
-    const maleHeight = toMeters(1800)
+    const maleHeight = toMeters(MALE_HEIGHT_MM)
     const maleWidth = maleHeight * ASPECT_RATIO
 
-    const femaleHeight = toMeters(1650)
+    const femaleHeight = toMeters(FEMALE_HEIGHT_MM)
     const femaleWidth = femaleHeight * ASPECT_RATIO
 
     const currentHeight = gender === "male" ? maleHeight : femaleHeight
 
     useFrame(({ camera }) => {
-        if (groupRef.current) {
-            const camPos = camera.position
-            groupRef.current.rotation.y = Math.atan2(
-                camPos.x - groupRef.current.position.x,
-                camPos.z - groupRef.current.position.z
-            )
-        }
+        if (!groupRef.current) return
+        
+        groupRef.current.rotation.y = Math.atan2(
+            camera.position.x - groupRef.current.position.x,
+            camera.position.z - groupRef.current.position.z
+        )
     })
 
     return (
-        <group ref={groupRef} position={[0, 0, 1.4]}>
+        <group ref={groupRef} position={COMPONENT_POSITION}>
             <DreiImage
                 url="/silhouette-01.svg"
                 scale={[maleWidth, maleHeight]}
@@ -53,20 +58,33 @@ const HumanScale = ({ gender, dimensions }: HumanScaleProps) => {
 
             {dimensions && (
                 <group>
-                    <Html position={[0.4, currentHeight / 2, 0]} center>
+                    <Html position={[DIMENSION_OFFSET, currentHeight / 2, 0]} center>
                         <div className='bg-black/60 whitespace-nowrap backdrop-blur-md text-white px-2 flex flex-row justify-center py-1 rounded border border-white/20 text-[10px] text-[clamp(10px,1vw,14px)] max-w-[200px]'>
                             <span>{Math.round(toMilimeters(currentHeight))} mm</span>
                         </div>
                     </Html>
-                    <Line color='black' points={[[0.4, 0, 0], [0.4, currentHeight, 0]]} lineWidth={1} />
-                    <Line color='black' points={[[0, 0, 0], [0.4, 0, 0]]} lineWidth={1} />
-                    <Line color='black' points={[[0, currentHeight, 0], [0.4, currentHeight, 0]]} lineWidth={1} />
+                    <Line 
+                        color={LINE_COLOR} 
+                        lineWidth={LINE_WIDTH} 
+                        points={[[DIMENSION_OFFSET, 0, 0], [DIMENSION_OFFSET, currentHeight, 0]]} 
+                    />
+                    <Line 
+                        color={LINE_COLOR} 
+                        lineWidth={LINE_WIDTH} 
+                        points={[[0, 0, 0], [DIMENSION_OFFSET, 0, 0]]} 
+                    />
+                    <Line 
+                        color={LINE_COLOR} 
+                        lineWidth={LINE_WIDTH} 
+                        points={[[0, currentHeight, 0], [DIMENSION_OFFSET, currentHeight, 0]]} 
+                    />
                 </group>
             )}
         </group>
     )
 }
-useTexture.preload("/silhouette-01.svg");
-useTexture.preload("/silhouette-02.svg");
 
-export default HumanScale;
+useTexture.preload("/silhouette-01.svg")
+useTexture.preload("/silhouette-02.svg")
+
+export default HumanScale
