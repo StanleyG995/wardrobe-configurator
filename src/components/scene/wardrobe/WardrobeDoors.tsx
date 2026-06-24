4f535c35c11b97a0344c0d23fecbc35841981624
Stanislaw <goral.stanislaw95@gmail.com>
2026-06-24 06:50:57 +0200

    refactor: wardrobe doors component cleanup

--- src/components/scene/wardrobe/WardrobeDoors.tsx
+++ src/components/scene/wardrobe/WardrobeDoors.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from "react" // <-- WAŻNE: Musisz dodać ten import na górze pliku
+import React from "react"
 import Door from "@/components/scene/wardrobe/Door"
 import { toMeters } from "@/helpers/unitConverter"
 import { WardrobeData } from "@/types/WardrobeProps"
@@ -8,6 +8,10 @@ import { WardrobeData } from "@/types/WardrobeProps"
 interface WardrobeDoorsProps extends WardrobeData {
     isOpen: boolean
 }
+
+const TOP_BAY_DOORS_BREAKPOINT = 2300
+const TOP_BAY_HEIGHT = 1900
+const DEFAULT_HINGE_SIDE = "left"
 
 const WardrobeDoors = ({
     width,
@@ -19,59 +23,51 @@ const WardrobeDoors = ({
 }: WardrobeDoorsProps) => {
     const segmentCount = segments.length > 0 ? segments.length : 1
     const compartmentWidth = width / segmentCount
-    const topBayDoorsBreakpoint = 2300
-    const topBayHeight = 1900
+    const startX = -width / 2
+    const halfThickness = boardThickness / 2
+    const topBayOffset = TOP_BAY_HEIGHT - halfThickness
 
     return (
         <>
             {segments.map((segment, idx) => {
-                const startX = -width / 2
-                const segmentX =
-                    startX +
-                    idx * (compartmentWidth) +
-                    compartmentWidth / 2
+                const segmentX = startX + idx * compartmentWidth + compartmentWidth / 2
+                const doorSide = segment.doorPosition || DEFAULT_HINGE_SIDE
+                const positionX = toMeters(segmentX)
+
+                const baseDoorProps = {
+                    width: compartmentWidth,
+                    depth,
+                    boardThickness,
+                    isOpen,
+                    hingeSide: doorSide,
+                    handleSide: doorSide,
+                }
 
                 return (
                     <React.Fragment key={`segment-door-wrapper-${segment.id}`}>
-                        {height < topBayDoorsBreakpoint ? (
-                            <group position={[toMeters(segmentX), 0, 0]}>
+                        {height < TOP_BAY_DOORS_BREAKPOINT ? (
+                            <group position={[positionX, 0, 0]}>
                                 <Door
-                                    width={compartmentWidth}
+                                    {...baseDoorProps}
                                     height={height}
-                                    depth={depth}
-                                    boardThickness={boardThickness}
-                                    isOpen={isOpen}
-                                    hingeSide={segment.doorPosition || 'left'}
-                                    handleSide={segment.doorPosition || 'left'}
                                     topOffset={0}
                                 />
                             </group>
                         ) : (
                             <>
-                            
-                                <group position={[toMeters(segmentX), 0, 0]}>
+                                <group position={[positionX, 0, 0]}>
                                     <Door
-                                        width={compartmentWidth}
-                                        height={height - (height - topBayHeight) - boardThickness / 2}
-                                        depth={depth}
-                                        boardThickness={boardThickness}
-                                        isOpen={isOpen}
-                                        hingeSide={segment.doorPosition || 'left'}
-                                        handleSide={segment.doorPosition || 'left'}
+                                        {...baseDoorProps}
+                                        height={height - (height - TOP_BAY_HEIGHT) - halfThickness}
                                         topOffset={0}
                                     />
                                 </group>
-                         
-                                <group position={[toMeters(segmentX), 0, 0]}>
+                                 
+                                <group position={[positionX, 0, 0]}>
                                     <Door
-                                        width={compartmentWidth}
-                                        height={height - topBayHeight + boardThickness / 2}
-                                        depth={depth}
-                                        boardThickness={boardThickness}
-                                        isOpen={isOpen}
-                                        hingeSide={segment.doorPosition || 'left'}
-                                        handleSide={segment.doorPosition || 'left'}
-                                        topOffset={topBayHeight - boardThickness / 2}
+                                        {...baseDoorProps}
+                                        height={height - TOP_BAY_HEIGHT + halfThickness}
+                                        topOffset={topBayOffset}
                                     />
                                 </group>
                             </>
