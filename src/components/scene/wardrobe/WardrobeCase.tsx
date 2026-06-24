dd3fc67f1d3ae3d2aa9d0279ec2ded2a5950a3fd
Stanislaw <goral.stanislaw95@gmail.com>
2026-06-24 06:53:44 +0200

    refactor: wardrobe case component cleanup

--- src/components/scene/wardrobe/WardrobeCase.tsx
+++ src/components/scene/wardrobe/WardrobeCase.tsx
@@ -1,68 +1,77 @@
+"use client"
+
 import Board from "@/components/scene/wardrobe/Board"
 import type { WardrobeDimensions } from "@/types/WardrobeProps"
 
-const WardrobeCase = (wardrobe: WardrobeDimensions) => {
-
-	const boardGap = 0
+const WardrobeCase = ({
+    width,
+    height,
+    depth,
+    boardThickness,
+    backBoardThickness,
+}: WardrobeDimensions) => {
+    const halfThickness = boardThickness / 2
+    const innerHeight = height - 2 * boardThickness
+    const sideX = width / 2 - halfThickness
+    const centerY = height / 2
 
     return (
         <group position={[0, 0.001, 0]}>
-					
-					<Board
-						name='wardrobe-bottom'
-						w={wardrobe.width - boardGap}
-						h={wardrobe.boardThickness - boardGap}
-						d={wardrobe.depth - boardGap}
-						x={0}
-						y={wardrobe.boardThickness / 2}
-						z={0}
-						rotation={[0, 0, 0]}
-					/>
+            <Board
+                name='wardrobe-bottom'
+                w={width}
+                h={boardThickness}
+                d={depth}
+                x={0}
+                y={halfThickness}
+                z={0}
+                rotation={[0, 0, 0]}
+            />
 
-					<Board
-						name='wardrobe-top'
-						w={wardrobe.width - boardGap}
-						h={wardrobe.boardThickness - boardGap}
-						d={wardrobe.depth - boardGap}
-						x={0}
-						y={wardrobe.height - wardrobe.boardThickness / 2}
-						z={0}
-						rotation={[0, 0, 0]}
-					/>
+            <Board
+                name='wardrobe-top'
+                w={width}
+                h={boardThickness}
+                d={depth}
+                x={0}
+                y={height - halfThickness}
+                z={0}
+                rotation={[0, 0, 0]}
+            />
 
-					<Board
-						name='wardrobe-side-left'
-						w={wardrobe.boardThickness - boardGap}
-						h={wardrobe.height - 2 * wardrobe.boardThickness - boardGap}
-						d={wardrobe.depth - boardGap}
-						x={wardrobe.width / 2 - wardrobe.boardThickness / 2}
-						y={wardrobe.height / 2}
-						z={0}
-						rotation={[0, 0, 0]}
-					/>
+            <Board
+                name='wardrobe-side-left'
+                w={boardThickness}
+                h={innerHeight}
+                d={depth}
+                x={sideX}
+                y={centerY}
+                z={0}
+                rotation={[0, 0, 0]}
+            />
 
-					<Board
-						name='wardrobe-side-right'
-						w={wardrobe.boardThickness - boardGap}
-						h={wardrobe.height - 2 * wardrobe.boardThickness - boardGap}
-						d={wardrobe.depth - boardGap}
-						x={-(wardrobe.width / 2 - wardrobe.boardThickness / 2)}
-						y={wardrobe.height / 2}
-						z={0}
-						rotation={[0, 0, 0]}
-					/>
+            <Board
+                name='wardrobe-side-right'
+                w={boardThickness}
+                h={innerHeight}
+                d={depth}
+                x={-sideX}
+                y={centerY}
+                z={0}
+                rotation={[0, 0, 0]}
+            />
 
-					<Board
-						name='wardrobe-back'
-						w={wardrobe.width - 2 * wardrobe.boardThickness - boardGap}
-						h={wardrobe.height - 2 * wardrobe.boardThickness - boardGap}
-						d={wardrobe.backBoardThickness - boardGap}
-						x={0}
-						y={wardrobe.height / 2}
-						z={-(wardrobe.depth / 2) + wardrobe.backBoardThickness}
-						rotation={[0, 0, 0]}
-					/>
-                </group>
+            <Board
+                name='wardrobe-back'
+                w={width - 2 * boardThickness}
+                h={innerHeight}
+                d={backBoardThickness}
+                x={0}
+                y={centerY}
+                z={-(depth / 2) + backBoardThickness}
+                rotation={[0, 0, 0]}
+            />
+        </group>
     )
 }
 
