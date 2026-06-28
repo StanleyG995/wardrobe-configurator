8586fd7787cf8fb6643a2a245ee258c296a752ab
Stanley995 <goral.stanislaw95@gmail.com>
2026-06-28 09:02:55 +0200

    refactor: change type structure

--- src/store/useWardrobeStore.ts
+++ src/store/useWardrobeStore.ts
@@ -1,44 +1,110 @@
-import { create } from 'zustand';
-import { temporal } from 'zundo';
-import { WardrobeData, WardrobeDimensions } from '../types/WardrobeProps';
+import { create } from "zustand";
+import { temporal } from "zundo";
+
+export interface Wardrobe{
+    dimension: {width: number;
+    height: number;
+    depth: number;}
+    boardThickness: number;
+    backBoardThickness: number;
+    segments: {
+        id: string;
+        type: 'shelves' | 'hanger' | 'empty';
+        shelves: string[];
+        doorPosition: 'left' | 'right';
+    }[];
+    activeSegmentIdx: number | null;
+    price: number;
+    caseMaterial: string;
+    doorMaterial: string;
+    updateDimension: (key: keyof Wardrobe['dimension'], value: number) => void;
+    setActiveSegmentIdx: (idx: number | null) => void
+}
+
+
+export interface ViewportOptionsProps{
+    dimensionsVisible: boolean,
+    humanScaleVisible: boolean,
+    humanScaleGender: 'male' | 'female',
+    doorsOpen: boolean,
+    doorsVisible: boolean,
+    floorVisible: boolean
+}
 
 export interface WardrobeState {
-    wardrobe: WardrobeData;
-    price: number;
-    activeSegmentIdx: number | null; // <-- Stan już tu był
-    updateDimension: (key: keyof WardrobeDimensions, value: number) => void;
-    // DODAJEMY AKCJĘ DO INTERFEJSU:
-    setActiveSegmentIdx: (idx: number | null) => void; 
+    wardrobe: Wardrobe;
+    viewportOptions: ViewportOptionsProps;
 }
 
 export const useWardrobeStore = create<WardrobeState>()(
-    temporal((set) => ({
-        wardrobe: {
-            width: 1000,
-            height: 2000,
-            depth: 600,
-            boardThickness: 18,
-            backBoardThickness: 5,
-            segments: [
-                { id: '1', type: 'shelves', shelves: [], doorPosition: 'left' },
-                { id: '2', type: 'hanger', shelves: [], doorPosition: 'right' }
-            ]
+  temporal(
+    (set) => ({
+      wardrobe: {
+        dimension: {
+          width: 1000,
+          height: 2000,
+          depth: 600
         },
-        price: 0,
-        activeSegmentIdx: null,
+        boardThickness: 18,
+        backBoardThickness: 5,
+        segments: [
+          { id: "1", type: "shelves", shelves: [], doorPosition: "left" },
+          { id: "2", type: "hanger", shelves: [], doorPosition: "right" },
+        ],
+      },
+      price: 0,
+      activeSegmentIdx: null,
 
-        updateDimension: (key: keyof WardrobeDimensions, value: number) => 
-            set((state) => ({
-                wardrobe: {
-                    ...state.wardrobe,
-                    [key]: value
-                }
-            })),
+      viewportOptions: {
+        dimensionsVisible: true,
+        humanScaleVisible: true,
+        humanScaleGender: "male",
+        doorsOpen: true,
+        doorsVisible: true,
+        floorVisible: true,
+      },
 
+      updateDimension: (key: keyof Wardrobe['dimension'], value: number) =>
+        set((state) => ({
+          wardrobe: {
+            ...state.wardrobe,
+            dimension: {
+              ...state.wardrobe.dimension,
+              [key]: value
+            }
+          },
+        })),
+
+      setActiveSegmentIdx: (idx: number | null) =>
+        set((state) => ({
+          wardrobe: {
+            ...state.wardrobe,
+            activeSegmentIdx: idx
+          }
+        })),
+
+        handleViewportToggle: (name: keyof ViewportOptionsProps) => {
+    set((state) => ({
        
-        setActiveSegmentIdx: (idx: number | null) => 
-            set(() => ({ 
-                activeSegmentIdx: idx 
-            }))
+        ...state, 
+       
+        viewportOptions: {
+            ...state.viewportOptions,
+            [name]: !state.viewportOptions[name]
+        }
     }))
+},
+
+        
+        else {
+            setViewportOptions(prev => ({
+                ...prev,
+                humanScaleGender: 'male'
+            }))
+        }
+    }),
+    {
+      partialize: (state) => ({ wardrobe: state.wardrobe }),
+    },
+  ),
 );
