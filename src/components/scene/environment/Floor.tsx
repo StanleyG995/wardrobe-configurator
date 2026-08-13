66c14fe2e0f4c2622a9bb91f9f73ac4128508c0a
Stanlej95 <goral.stanislaw95@gmail.com>
2026-08-13 10:20:31 +0200

    fix: floor texture path

--- src/components/scene/environment/Floor.tsx
+++ src/components/scene/environment/Floor.tsx
@@ -7,28 +7,29 @@ const FLOOR_SCALE: [number, number, number] = [10, 10, 1]
 const FLOOR_POSITION: [number, number, number] = [0, -0.001, 0]
 const FLOOR_ROTATION: [number, number, number] = [-Math.PI / 2, 0, 0]
 const TEXTURE_REPEAT: [number, number] = [10, 10]
+const TEXTURE_PATH = "/textures/floor.webp"
 
 const Floor = () => {
-	const floorTexture = useTexture("/textures/floor.webp", texture => {
-		texture.wrapS = THREE.RepeatWrapping
-		texture.wrapT = THREE.RepeatWrapping
-		texture.repeat.set(...TEXTURE_REPEAT)
-		texture.colorSpace = THREE.SRGBColorSpace
-	})
+    const floorTexture = useTexture(TEXTURE_PATH, texture => {
+        texture.wrapS = THREE.RepeatWrapping
+        texture.wrapT = THREE.RepeatWrapping
+        texture.repeat.set(...TEXTURE_REPEAT)
+        texture.colorSpace = THREE.SRGBColorSpace
+    })
 
-	return (
-		<mesh
-			name='floor'
-			scale={FLOOR_SCALE}
-			position={FLOOR_POSITION}
-			rotation={FLOOR_ROTATION}
-			receiveShadow>
-			<planeGeometry />
-			<meshStandardMaterial map={floorTexture} side={THREE.DoubleSide} />
-		</mesh>
-	)
+    return (
+        <mesh
+            name='floor'
+            scale={FLOOR_SCALE}
+            position={FLOOR_POSITION}
+            rotation={FLOOR_ROTATION}
+            receiveShadow>
+            <planeGeometry />
+            <meshStandardMaterial map={floorTexture} side={THREE.DoubleSide} />
+        </mesh>
+    )
 }
 
-useTexture.preload("/floor.webp")
+useTexture.preload(TEXTURE_PATH)
 
 export default Floor
